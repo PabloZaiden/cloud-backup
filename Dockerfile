4ecb13b78bcec654d26949b8e0261ef85bedb247
Pablo Zaidenvoren <{ID}+{username}@users.noreply.github.com>
2023-01-18 09:16:50 -0500

    docker

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM ubuntu:22.10
+
+RUN apt-get update
+RUN apt-get install -y wget zip
+RUN mkdir /azcopy
+WORKDIR /azcopy
+RUN wget https://aka.ms/downloadazcopy-v10-linux
+RUN tar -xf downloadazcopy-v10-linux
+RUN rm downloadazcopy-v10-linux
+RUN mv $(ls)/azcopy /usr/bin/azcopy 
+WORKDIR /
+RUN rm -rf /azcopy
+
+RUN mkdir /data
+
+ENV AZURE_STORAGE_ACCOUNT=""
+ENV AZURE_STORAGE_ACCOUNT_CONTAINER=""
+ENV AZURE_STORAGE_SAS_TOKEN=""
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+# function that validates that a variable is set
+function validate_is_set {
+    if [ -z "$1" ]; then
+        echo "ERROR: $2 is not set"
+        exit 1
+    fi
+}
+
+# validate that all required variables are set
+validate_is_set "$AZURE_STORAGE_ACCOUNT" "AZURE_STORAGE_ACCOUNT"
+validate_is_set "$AZURE_STORAGE_ACCOUNT_CONTAINER" "AZURE_STORAGE_ACCOUNT_CONTAINER"
+validate_is_set "$AZURE_STORAGE_SAS_TOKEN" "AZURE_STORAGE_SAS_TOKEN"
+
+# get yyyy-mm-dd-hh-mm-ss date
+currentDate=$(date +%Y-%m-%d-%H-%M-%S)
+zipFileName="backup-$currentDate.zip"
+
+echo "Zipping files recursively in /data..."
+zip -r "/$zipFileName" /data
+
+echo "Uploading zip file to Azure Storage with azcopy..."
+azcopy copy "/$zipFileName" "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$AZURE_STORAGE_ACCOUNT_CONTAINER/$zipFileName?$AZURE_STORAGE_SAS_TOKEN"
+
+echo "Cleaning up..."
+rm "/$zipFileName"
+
+echo "Done!"
